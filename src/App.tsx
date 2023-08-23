1c8a13205e857a8b54920e0cdd1b29ff50ec6e6d
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-23 12:28:50 +0300

    loader refactored into a separate function

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,14 @@ import {
 import MainLayout from "./components/MainLayout";
 import Editor from "./pages/Editor";
 import Home from "./pages/Home";
+import {loader as homeLoader} from "./pages/Home";
 
 // Create a browser router with a main layout 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<MainLayout />}>
 			<Route path="/" element={<Navigate to="/images" replace={true} />} />
-			<Route path="/images" element={<Home />} />
+			<Route path="/images" element={<Home />} loader={homeLoader}/>
 			<Route path="/images/:imageId/edit" element={<Editor />} />
 		</Route>
 	)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import { Image } from "@/common/types";
+import ImageList from "@/components/ImageList";
+
+export const loader = async () => {
+
+	const response = await fetch("https://picsum.photos/v2/list");
+	const data = await response.json();
+
+	return data;
+	
+};
+
 export default function Home() {
+	const images = (useLoaderData() as Image[]) || [];
+
+	console.log("Images", images);
 
 	return (
 		<div>
-			<div>
-				HOME
-			</div>
+			<ImageList images={images} />			
 		</div>
 	);
-
-};
+}
