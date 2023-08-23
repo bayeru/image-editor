8fd1313cd9b8f194a8a57b22d48dc0b891be4feb
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-23 10:39:06 +0300

    Added comment for the react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+	Navigate,
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Editor from "./pages/Editor";
 import Home from "./pages/Home";
 
-let router = createBrowserRouter(createRoutesFromElements(
-	<Route path="/" element={<MainLayout />}>
-		<Route path="/" element={<Navigate to="/images" replace={true} />} />
-		<Route path="/images" element={<Home />} />
-		<Route path="/images/:imageId/edit" element={<Editor />} />
-	</Route>
-));
+// Create a browser router with a main layout 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<MainLayout />}>
+			<Route path="/" element={<Navigate to="/images" replace={true} />} />
+			<Route path="/images" element={<Home />} />
+			<Route path="/images/:imageId/edit" element={<Editor />} />
+		</Route>
+	)
+);
 
 function App() {
 	return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
