05ac4b36b2443dacde0a0bee25aa189e1d11591b
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-23 16:13:14 +0300

    Added links to images

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -5,6 +5,7 @@ import {
 	ImageListItemBar,
 } from "@mui/material";
 import { Image } from "@/common/types";
+import { Link } from "react-router-dom";
 
 interface ImageListProps {
 	images: Image[];
@@ -14,42 +15,43 @@ export default function ImageList({ images }: ImageListProps) {
 	return (
 		<MuiImageList cols={3} gap={16} sx={{ overflow: "hidden" }}>
 			{images.map((item) => (
-				<ImageListItem
-					key={item.id}
-					sx={{
-						cursor: "pointer",
-						borderRadius: 2,
-						overflow: "hidden"
-					}}
-				>
-					<img
-						src={`https://picsum.photos/id/${item.id}/367/267`}
-						srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-						alt={item.author}
-						loading="lazy"
-					/>
-					<ImageListItemBar						
+				<Link to={`/images/${item.id}/edit`} key={item.id}>
+					<ImageListItem
 						sx={{
-							background:
-								"linear-gradient(to top, rgba(1,1,1,0.7) 0%, " +
-								"rgba(1,1,1,0.3) 70%, rgba(1, 1, 1, 0) 100%)",
-							"& .MuiImageListItemBar-titleWrap": {
-								display: "flex",
-								justifyContent: "space-between",
-								alignItems: "center",
-							},
-							"& .MuiImageListItemBar-title": {
-								fontSize: "1.2rem",
-							},							
-							"& .MuiImageListItemBar-subtitle": {
-								fontSize: "1rem",
-								opacity: "0.7"
-							},
+							cursor: "pointer",
+							borderRadius: 2,
+							overflow: "hidden",
 						}}
-						title={item.author}
-						subtitle={`#${item.id}`}
-					/>
-				</ImageListItem>
+					>
+						<img
+							src={`https://picsum.photos/id/${item.id}/367/267`}
+							srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+							alt={item.author}
+							loading="lazy"
+						/>
+						<ImageListItemBar
+							sx={{
+								background:
+									"linear-gradient(to top, rgba(1,1,1,0.7) 0%, " +
+									"rgba(1,1,1,0.3) 70%, rgba(1, 1, 1, 0) 100%)",
+								"& .MuiImageListItemBar-titleWrap": {
+									display: "flex",
+									justifyContent: "space-between",
+									alignItems: "center",
+								},
+								"& .MuiImageListItemBar-title": {
+									fontSize: "1.2rem",
+								},
+								"& .MuiImageListItemBar-subtitle": {
+									fontSize: "1rem",
+									opacity: "0.7",
+								},
+							}}
+							title={item.author}
+							subtitle={`#${item.id}`}
+						/>
+					</ImageListItem>
+				</Link>
 			))}
 		</MuiImageList>
 	);
