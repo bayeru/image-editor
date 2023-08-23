ea0c8187ccd88fa4a9cdfdd5468afae09edd53ad
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-23 11:09:50 +0300

    NavBar refactored into a new component

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,50 +1,13 @@
-import {
-	AppBar,
-	Box,
-	Button,
-	Toolbar,
-	Typography,
-} from "@mui/material";
-import BurstModeIcon from "@mui/icons-material/BurstMode";
 import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 /**
- * Main layout for the application. Includes the app bar and the <Outlet /> for the react router.
+ * Main layout for the application. Includes the nav bar and the <Outlet /> for the react router.
  */
 export default function MainLayout() {
 	return (
 		<>
-			<AppBar
-				position={"fixed"}
-				sx={{
-					margin: 0,
-					backgroundColor: "#fcfcfc",
-					borderBottom: "1px solid rgba(0, 0, 0, 0.07)",
-				}}
-				elevation={0}
-			>
-				<Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-					<Box sx={{ display: "flex", alignItems: "center" }}>
-						<Button
-							onClick={() => {}}
-							sx={{
-								marginRight: 1,
-							}}
-						>
-							<BurstModeIcon />
-							<Typography
-								sx={{
-									ml: 1,
-									variant: "h1",
-									textTransform: "none",
-								}}
-							>
-								ImagED
-							</Typography>
-						</Button>
-					</Box>
-				</Toolbar>
-			</AppBar>
+			<NavBar />
 			<div>
 				<Outlet />
 			</div>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import BurstModeIcon from "@mui/icons-material/BurstMode";
+
+export default function NavBar() {
+	return (
+		<AppBar
+			position={"fixed"}
+			sx={{
+				margin: 0,
+				backgroundColor: "#fcfcfc",
+				borderBottom: "1px solid rgba(0, 0, 0, 0.07)",
+			}}
+			elevation={0}
+		>
+			<Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+				<Box sx={{ display: "flex", alignItems: "center" }}>
+					<Button
+						onClick={() => {}}
+						sx={{
+							marginRight: 1,
+						}}
+					>
+						<BurstModeIcon />
+						<Typography
+							sx={{
+								ml: 1,
+								variant: "h1",
+								textTransform: "none",
+							}}
+						>
+							ImagED
+						</Typography>
+					</Button>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
+}
