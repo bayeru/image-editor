0f6544ac81bfa8beed627a1edbe737510ef5db00
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-25 16:46:50 +0300

    A global loading screen added to the main layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,15 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "./NavBar";
 import { Box } from "@mui/material";
+import MainFallback from "./MainFallback";
 
 /**
  * Main layout for the application. Includes the nav bar and the <Outlet /> for the react router.
  */
 export default function MainLayout() {
+
+	const navigation = useNavigation();
+	const isLoading = navigation.state === "loading";
+	console.log("Navigation", navigation);
+
 	return (
 		<>
 			<NavBar />
 			<Box>
+				{isLoading && <MainFallback />}
 				<Outlet />
 			</Box>
 		</>
