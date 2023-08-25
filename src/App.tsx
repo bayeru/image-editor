7ac0ab92170a8e7c744f0da883945eedbdc773c2
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-25 15:35:53 +0300

    An Error page is added

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,13 @@ import Home from "./pages/Home";
 import { loader as homeLoader } from "./pages/Home";
 import { loader as editorLoader } from "./pages/Editor";
 import MainFallback from "./components/MainFallback";
+import Error from "./components/Error";
 
 // Create a browser router with a main layout
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<>
-			<Route path="/" element={<MainLayout />}>
+			<Route path="/" element={<MainLayout />} errorElement={<Error />}>
 				<Route path="/" element={<Navigate to="/images?page=1" replace={true} />} />
 				<Route path="/images" element={<Home />} loader={homeLoader} />
 				<Route path="/images/:imageId/edit" element={<Editor />} loader={editorLoader} />
---
+++ src/components/Error.tsx
@@ -0,0 +1,17 @@
+import { Container } from "@mui/material";
+
+export default function Error() {
+	return (
+		<Container
+			sx={{
+				display: "flex",
+				width: "100%",
+				height: "100vh",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<h1>404 - Page Not Found</h1>
+		</Container>
+	);
+}
