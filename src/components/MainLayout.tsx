73448220f02abac5878a21dd85a19c2490524f7b
bayeru <1945179+bayeru@users.noreply.github.com>
2023-08-27 02:06:33 +0300

    Unnecessary comments removed

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,9 +3,6 @@ import NavBar from './NavBar';
 import { Box } from '@mui/material';
 import LoadingScreen from './LoadingScreen';
 
-/**
- * Main layout for the application. Includes the nav bar and the <Outlet /> for the react router.
- */
 export default function MainLayout() {
 	const navigation = useNavigation();
 	const isLoading = navigation.state === 'loading';
